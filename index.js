ef915aeecf49140600e5aaad052c6aa9783849bb
Vaibhav Chanana <vaibhavchanana@yahoo.co.in>
2020-06-02 00:45:58 +0530

    Production ready changes

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ const bot = new TelegramBot(process.env.token, {polling: true});
 bot.on("polling_error", (err) => console.log(err));
 
 agenda.define('send-message', {priority: 'high'}, async job => {
-    console.log("Send Message running");
+    // console.log("Send Message running");
     const {chatId, replyMessage, replyId} = job.attrs.data;
     await bot.sendMessage(chatId, replyMessage.reminderMessage, {"reply_to_message_id": replyId});
 })
 
 agenda.define('clean-database', async job => {    
     const numRemoved = await agenda.cancel({name: 'send-message', nextRunAt: null});
-    console.log(numRemoved + " have been removed");
+    // console.log(numRemoved + " have been removed");
 });
 
 // agenda.schedule('in 10 seconds', 'task', {"id": 123});
 (async function() { // IIFE to give access to async/await
     await agenda.start();
-    await agenda.every('1 minute', 'clean-database');
+    await agenda.every('10 days', 'clean-database');
 })();
 
 async function setReminder(msg, replyMessage, incomingMessage, replyId) {
@@ -32,8 +32,8 @@ async function setReminder(msg, replyMessage, incomingMessage, replyId) {
     if(result !== null) {
         if(result[3] !== undefined) {
             bot.sendMessage(chatId, replyMessage.confirmation);
-            console.log("Scheduling message in " + result[1]);
-            await agenda.schedule('in ' + result[1] + 'seconds', 'send-message', {"chatId": chatId, "replyMessage": replyMessage, "replyId": replyId});
+            // console.log("Scheduling message in " + result[1]);
+            await agenda.schedule('in ' + result[1] + 'minutes', 'send-message', {"chatId": chatId, "replyMessage": replyMessage, "replyId": replyId});
         } else if (result[4] !== undefined) {
             bot.sendMessage(chatId, replyMessage.confirmation);
             await agenda.schedule('in ' + result[1] + 'hours', 'send-message', {"chatId": chatId, "replyMessage": replyMessage, "replyId": replyId});
