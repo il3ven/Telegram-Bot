a02eb0499cd7587199af5b9edba9d29e49dbbc6d
Vaibhav Chanana <vaibhavchanana@yahoo.co.in>
2020-06-01 23:59:58 +0530

    Agenda added

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
-// const config = require('./config.json')
+//const config = require('./config.json')
 const TelegramBot = require('node-telegram-bot-api');
+const Agenda = require('agenda');
+
+const mongoConnectionString = process.env.MONGODB_URL;
+const agenda = new Agenda({db: {address: mongoConnectionString}});
 
 const bot = new TelegramBot(process.env.token, {polling: true});
 bot.on("polling_error", (err) => console.log(err));
 
-function setReminder(msg, replyMessage, incomingMessage, replyId) {
+agenda.define('send-message', {priority: 'high'}, async job => {
+    // console.log("Send Message running");
+    const {chatId, replyMessage, replyId} = job.attrs.data;
+    await bot.sendMessage(chatId, replyMessage.reminderMessage, {"reply_to_message_id": replyId});
+})
+
+agenda.define('clean-database', async job => {    
+    const numRemoved = await agenda.cancel({name: 'send-message'});
+    // console.log(numRemoved + " have been removed");
+});
+
+// agenda.schedule('in 10 seconds', 'task', {"id": 123});
+(async function() { // IIFE to give access to async/await
+    await agenda.start();
+    await agenda.every('1 minute', 'clean-database');
+})();
+
+async function setReminder(msg, replyMessage, incomingMessage, replyId) {
     let chatId = msg.chat.id;
     const result = /.+ ([0-9]+) ((m|mins|min|minutes|minute)|(h|hour|hours)|(d|days|day))$/.exec(incomingMessage);
 
     if(result !== null) {
         if(result[3] !== undefined) {
             bot.sendMessage(chatId, replyMessage.confirmation);
-            setTimeout(() => bot.sendMessage(chatId, replyMessage.reminderMessage, {"reply_to_message_id": replyId}), result[1]*60000);
+            console.log("Scheduling message in " + result[1]);
+            await agenda.schedule('in ' + result[1] + 'seconds', 'send-message', {"chatId": chatId, "replyMessage": replyMessage, "replyId": replyId});
         } else if (result[4] !== undefined) {
             bot.sendMessage(chatId, replyMessage.confirmation);
-            setTimeout(() => bot.sendMessage(chatId, replyMessage.reminderMessage, {"reply_to_message_id": replyId}), result[1]*3600000);
+            await agenda.schedule('in ' + result[1] + 'hours', 'send-message', {"chatId": chatId, "replyMessage": replyMessage, "replyId": replyId});
         } else if (result[5] !== undefined) {
             bot.sendMessage(chatId, replyMessage.confirmation);
-            setTimeout(() => bot.sendMessage(chatId, replyMessage.reminderMessage, {"reply_to_message_id": replyId}), result[1]*86400000);
+            await agenda.schedule('in ' + result[1] + 'days', 'send-message', {"chatId": chatId, "replyMessage": replyMessage, "replyId": replyId});
         } else {
             bot.sendMessage(chatId, replyMessage.errorMessage);
         }
