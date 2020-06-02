e5ba6f37bf885d46d71793d1480916b179e7db02
Vaibhav Chanana <vaibhavchanana@yahoo.co.in>
2020-06-02 21:09:22 +0530

    Regular stats feature added

--- data.js
+++ data.js
@@ -5,6 +5,10 @@ module.exports.push = function (json) {
     data.push(json);
 }
 
+module.exports.setArray = function (newArray) {
+    data = newArray;
+}
+
 module.exports.getArray = function () {
    return data;
 }
